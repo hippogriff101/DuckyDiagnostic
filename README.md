3339c24a2221fadc65f3d2a9db458edceea8b304
Freddie Yershon <143739931+hippogriff101@users.noreply.github.com>
2025-11-01 16:52:24 +0000

    Update main.py and edit README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # DuckyDiagnostic
-A small troubleshooter using the PicoDucky 
+
+A small troubleshooter using the PicoDucky
+
+This is being subbmitted to Hack Club's YSWS [PicoDucky](picoducky.hackclub.com). A PicoDucky is a custon PCB that acts as a HID device and is open soursed on Github [here](https://github.com/Outdatedcandy92/PicoDucky).
+
+## My project
+
+When the USB is plugged in, press the button one to three times to signal the OS. This opens a terminal and runs several commands to gather information about the computer and automatically perform OS-specific troubleshooting steps. 
+
+### Commands to use
+
+#### Windows
+|   Win + r
+|   wt
+|   Enter
+
+#### Mac
+|   Command + Space
+|   terminal
+|   Enter
+
+#### Linux
+|   Ctrl + Alt + T
+
--- main.py
+++ main.py
@@ -1,4 +1,45 @@
 import keyboard
 import time
 
-keyboard.write("Hello, World!")
+def main():
+    while True:
+        buttoninput = int(input("Press 1 for windows, 2 for linux and 3 for mac: "))
+
+        if buttoninput == 1:
+            windows()
+            break
+        elif buttoninput == 2:
+            linux()
+            break
+        elif buttoninput == 3:
+            mac()
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+    time.sleep(1)
+    keyboard.write("whoami > whoami.txt")
+    keyboard.press_and_release("enter")
+    time.sleep(0.5)
+    keyboard.write("cat whoami.txt")
+    keyboard.press_and_release("enter")
+
+def windows():
+
+    keyboard.press_and_release("win+r")
+    time.sleep(0.5)
+    keyboard.write("wt")
+    keyboard.press_and_release("enter")
+
+
+def linux():
+    keyboard.press_and_release("ctrl+alt+t")
+
+def mac():
+    keyboard.press_and_release("cmd+space")
+    time.sleep(0.5)
+    keyboard.write("Terminal")
+    keyboard.press_and_release("enter")
+
+if __name__ == "__main__":
+    main()
